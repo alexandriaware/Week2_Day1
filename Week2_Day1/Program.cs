ae9c4551042d938cad6a043437ebbc219a6c3edf
alexandriaware <ali.mercer92@gmail.com>
2017-10-02 11:19:26 -0400

    in class practice

--- Week2_Day1/Program.cs
+++ Week2_Day1/Program.cs
@@ -14,6 +14,20 @@ namespace Week2_Day1
             int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
 
             string[] studentNames = { "Raz", "Monica", "David", "Brian", "Ali" };
+
+            Console.WriteLine(studentNames[2]);
+
+            //Do it!
+            string[] vacationSpots = { "Disney", "Amsterdam", "Paris", "Geneva", "Dublin",
+                "Vegas", "Denver", "Portland", "Ann Arbor", "Cape Cod" };
+            Console.WriteLine(vacationSpots[0]);
+            Console.WriteLine(vacationSpots[9]);
+
+
+            int[] babyWhippersnappers = { 5, 3, 8 };
+            Console.WriteLine(babyWhippersnappers[0]);
+            Console.WriteLine(babyWhippersnappers[1]);
+            Console.WriteLine(babyWhippersnappers[2]);
         }
     }
 }
